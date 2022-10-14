0a131debad4c3341366d4db85393f831d23cd7e5
Liam <jlhs1001@gmail.com>
2022-10-14 11:15:54 -0400

    add LICENSE.txt and setup.cfg

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     license="MIT",
     author="jlhs1001 (aka Liam Seewald)",
     author_email="jlhsdev@gmail.com",
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/'
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/jlhs1001/ascii-to-html",
+    keywords="ANSI,HTML,python3,parse,parsing,convert,conversion,ansitohtml,Ansi,Html"
+             "translation,translate,ascii,fast,simple,easy,web,tool,python,html,ansi,ansi2html"
 )
